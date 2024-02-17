db4a0c0c99bb437ed99b1a62a72bf63fadc24900
nko <solowingrazgriz@outlook.com>
2024-02-17 12:47:24 +0900

    delete scaffolding junk

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+/* import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -20,4 +20,4 @@ const router = createRouter({
   ]
 })
 
-export default router
+export default router */
