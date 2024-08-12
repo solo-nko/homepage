81bfff159edfd17c73441740016e0f570768b414
solo-nko <solowingrazgriz@outlook.com>
2024-08-12 19:22:49 +0900

    Add keyboard shortcuts to CounterApp.vue

--- src/views/projects/CounterApp.vue
+++ src/views/projects/CounterApp.vue
@@ -1,27 +1,58 @@
 <template>
 	<div id="grid-container">
-		<h1 class="grid-row theme-text">Counter App</h1>
+		<div id="header-row" class="grid-row theme-text">
+			<h1>Counter App</h1>
+			<p>(Keyboard shortcuts in parenthesis)</p>
+		</div>
+
 		<div id="counterResult" class="grid-row theme-text">{{ count }}</div>
 
-		<BaseButton id="incrButton" class="theme-text enlarged-text" v-on:click="increaseCount(delta)">Increment</BaseButton>
-		<BaseButton id="decrButton" class="theme-text enlarged-text" v-on:click="decreaseCount(delta)">Decrement</BaseButton>
-		<BaseButton id="resetButton" class="theme-text enlarged-text" v-on:click="resetCount()">Reset to 0</BaseButton>
+		<BaseButton id="incrButton" class="theme-text enlarged-text" v-on:click="increaseCount(delta)">Increment <br>( + )
+		</BaseButton>
+		<BaseButton id="decrButton" class="theme-text enlarged-text" v-on:click="decreaseCount(delta)">Decrement <br>( - )
+		</BaseButton>
+		<BaseButton id="resetButton" class="theme-text enlarged-text" v-on:click="resetCount()">Reset to 0 <br>( Z )</BaseButton>
 
 		<div id="inputs-container" class="grid-row">
 			<label class="theme-text">Change by</label>
-			<input type="number" inputmode="numeric" value="1" min="1" v-on:change="changeDelta($event)">
+			<input type="number" inputmode="numeric" value="1" min="1" v-on:change="changeDelta($event)" ref="deltaInput">
 			<label for="" class="theme-text">Skip to</label>
-			<input type="number" inputmode="numeric" v-on:change="setCount($event)" />
+			<input type="number" inputmode="numeric" v-on:change="setCount($event)" ref="setInput" />
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import BaseButton from "@/components/common/BaseButton.vue";
-import { ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 
 const count = ref(0);
 const delta = ref(1);
+const deltaInput = ref<Element | null>(null);
+const setInput = ref<Element | null>(null);
+
+onMounted(() => {
+	document.addEventListener("keyup", keyHandler);
+});
+
+onUnmounted(() => {
+	document.removeEventListener("keyup", keyHandler);
+});
+
+// keyboard shortcuts
+function keyHandler(event: KeyboardEvent | undefined) {
+	console.log(document.activeElement);
+	console.log(deltaInput.value);
+	if (
+			event &&
+			document.activeElement != deltaInput.value &&
+			document.activeElement != setInput.value) {
+		if (event.key == "+") increaseCount(delta.value);
+		else if (event.key == "-") decreaseCount(delta.value);
+		else if (event.key == "z") resetCount();
+		console.log(event.key);
+	}
+}
 
 function increaseCount(incrementAmount = 1): void {
 	count.value += incrementAmount;
@@ -32,18 +63,23 @@ function decreaseCount(decrementAmount = 1): void {
 }
 
 function resetCount() {
+	// reset count
 	count.value = 0;
+	// reset "change by" value internally
 	delta.value = 1;
+	// reset "change by" field
+	const deltaField = deltaInput.value as HTMLInputElement;
+	if (deltaField) deltaField.value = String(delta.value);
 }
 
 function changeDelta(event: Event) {
 	const target = event.target as HTMLInputElement;
 	if (target) {
-		const targetValue = Number(target.value)
+		const targetValue = Number(target.value);
 		if (targetValue < 1) delta.value = 1;
 		else delta.value = targetValue;
 
-		target.value = String(delta.value)
+		target.value = String(delta.value);
 	}
 }
 
@@ -89,11 +125,18 @@ h1, #counterResult {
 	font-size: 4rem;
 }
 
-h1 {
+#header-row {
 	grid-area: header;
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+	h1 {
+		margin-bottom: 0;
+	}
+	p {
+		margin-top: 0;
+	}
 }
 
 #counterResult {
